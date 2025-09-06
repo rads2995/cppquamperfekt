07f32c27dbe65f58dabd67809b7b2eeba71174d9
Rodrigo Duran <rads2995@gmail.com>
2025-09-05 21:59:05 -0400

    Adding support for SYCL

--- bin/main.cpp
+++ bin/main.cpp
@@ -1,5 +1,11 @@
+#include <cassert>
+
 #include <cppquamperfekt/cppquamperfekt.hpp>
 
 int main() {
+    
+    int num = 2;
+    auto value = cppquamperfekt::test_func(num);
+    assert(value == 4);    
     return 0;
 }
--- src/include/cppquamperfekt/cppquamperfekt.hpp
+++ src/include/cppquamperfekt/cppquamperfekt.hpp
@@ -1,5 +1,19 @@
 #pragma once
 
+#include <expected>
+
+#include <sycl/sycl.hpp>
+
 namespace cppquamperfekt {
     
+enum class ReturnCode : int {
+    valid = 0,
+    invalid
+};
+
+template<typename T>
+auto test_func(T input) -> std::expected<T, ReturnCode> {
+    return input * input;
+}
+
 }

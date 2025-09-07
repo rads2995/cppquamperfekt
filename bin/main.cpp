e87ab2c29ec9bf5c0aeab7689964c7f73ef754e5
Rodrigo Duran <rads2995@gmail.com>
2025-09-07 17:28:35 -0400

    Adding testing using GTest and other fixes

--- bin/main.cpp
+++ bin/main.cpp
@@ -9,7 +9,8 @@ int main() {
 
     sycl::device d;
     try {
-        d = sycl::device(sycl::gpu_selector_v);
+        // d = sycl::device(sycl::gpu_selector_v);
+        d = sycl::device(sycl::default_selector_v);
     } catch (sycl::exception const& e) {
         d = sycl::device(sycl::cpu_selector_v);
     }
@@ -21,29 +22,24 @@ int main() {
         << "Platform: "
         << q.get_device().get_platform().get_info<sycl::info::platform::name>()
         << "\n";
-    
-    constexpr int num_ints = 1024 * 1024;
-    constexpr size_t num_bytes = num_ints * sizeof(int);
-    constexpr int alignment = 8;
 
-    // Alloc memory on host
-    auto src = std::aligned_alloc(alignment, num_bytes);
-    std::memset(src, 1, num_bytes);
-
-    // Alloc memory on device
-    auto dst = sycl::malloc_device<int>(num_ints, q);
-    q.memset(dst, 0, num_bytes).wait();
-
-    // Copy from host to device
-    auto event = q.memcpy(dst, src, num_bytes);
+    int* data = sycl::malloc_shared<int>(1024, q);
+    sycl::event event = q.parallel_for(1024, 
+        [=](sycl::id<1> idx) {
+            data[idx] = idx;
+        });
     event.wait();
+
+    for (int i = 0; i < 1024; i++) {
+        std::cout << "data[" << i << "] = " << data[i] << std::endl;
+    }
 
     auto start = event.get_profiling_info<sycl::info::event_profiling::command_start>();
     auto end = event.get_profiling_info<sycl::info::event_profiling::command_end>();
     
     std::cout << "Elapsed time: " << (end - start) / 1.0e9 << " seconds\n";
 
-    sycl::free(dst, q);
+    sycl::free(data, q);
 
     return 0;
 }

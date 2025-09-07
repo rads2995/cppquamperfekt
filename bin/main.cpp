f44082afc8f91eba1b2bb19d7ccfd8bfa7e6ca92
Rodrigo Duran <rads2995@gmail.com>
2025-09-06 23:55:24 -0400

    Example of allocating memory on host and copying to device while measuring how long it took.

--- bin/main.cpp
+++ bin/main.cpp
@@ -1,11 +1,49 @@
-#include <cassert>
+#include <vector>
+#include <iostream>
+
+#include <sycl/sycl.hpp>
 
 #include <cppquamperfekt/cppquamperfekt.hpp>
 
 int main() {
+
+    sycl::device d;
+    try {
+        d = sycl::device(sycl::gpu_selector_v);
+    } catch (sycl::exception const& e) {
+        d = sycl::device(sycl::cpu_selector_v);
+    }
+
+    sycl::property_list properties{sycl::property::queue::enable_profiling()};
+    sycl::queue q = sycl::queue(d, properties);
+
+    std::cout
+        << "Platform: "
+        << q.get_device().get_platform().get_info<sycl::info::platform::name>()
+        << "\n";
     
-    int num = 2;
-    auto value = cppquamperfekt::test_func(num);
-    assert(value == 4);    
+    constexpr int num_ints = 1024 * 1024;
+    constexpr size_t num_bytes = num_ints * sizeof(int);
+    constexpr int alignment = 8;
+
+    // Alloc memory on host
+    auto src = std::aligned_alloc(alignment, num_bytes);
+    std::memset(src, 1, num_bytes);
+
+    // Alloc memory on device
+    auto dst = sycl::malloc_device<int>(num_ints, q);
+    q.memset(dst, 0, num_bytes).wait();
+
+    // Copy from host to device
+    auto event = q.memcpy(dst, src, num_bytes);
+    event.wait();
+
+    auto start = event.get_profiling_info<sycl::info::event_profiling::command_start>();
+    auto end = event.get_profiling_info<sycl::info::event_profiling::command_end>();
+    
+    std::cout << "Elapsed time: " << (end - start) / 1.0e9 << " seconds\n";
+
+    sycl::free(dst, q);
+
     return 0;
 }

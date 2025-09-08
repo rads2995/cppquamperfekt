26389ceb0a8ccccfda594ff36e812686be1f2e4e
Rodrigo Duran <rads2995@gmail.com>
2025-09-07 23:50:11 -0400

    Example using a more complicated kernel and larger allocation for vectors

--- bin/main.cpp
+++ bin/main.cpp
@@ -7,6 +7,8 @@
 
 int main() {
 
+    constexpr int size = 1'000'000'000;
+    
     sycl::device d;
     try {
         d = sycl::device(sycl::gpu_selector_v);
@@ -22,23 +24,39 @@ int main() {
         << q.get_device().get_platform().get_info<sycl::info::platform::name>()
         << "\n";
 
-    int* data = sycl::malloc_shared<int>(1024, q);
-    sycl::event event = q.parallel_for(1024, 
-        [=](sycl::id<1> idx) {
-            data[idx] = idx;
-        });
-    event.wait();
 
-    for (int i = 0; i < 1024; i++) {
-        std::cout << "data[" << i << "] = " << data[i] << std::endl;
+    // USM allocator for data of type int in shared memory
+    using vec_alloc = sycl::usm_allocator<int, sycl::usm::alloc::shared>;
+
+    // Create allocator for device associated with queue
+    vec_alloc allocator(q);
+
+    // Create std::vector using this allocator
+    std::vector<int, vec_alloc> a(size, allocator), b(size, allocator), c(size, allocator);
+
+    // Get pointer(s) to vector(s) data for access in kernel
+    auto A = a.data();
+    auto B = b.data();
+    auto C = c.data();
+
+    for (int i = 0; i < size; i++) {
+        a[i] = i;
+        b[i] = i;
+        c[i] = i;
     }
 
+    sycl::event event = q.submit([&](sycl::handler &h) {
+        h.parallel_for(sycl::range<1>(size),
+            [=](sycl::id<1> idx) {
+                C[idx] = A[idx] + B[idx];
+            });
+    });
+    event.wait();
+    
     auto start = event.get_profiling_info<sycl::info::event_profiling::command_start>();
     auto end = event.get_profiling_info<sycl::info::event_profiling::command_end>();
     
     std::cout << "Elapsed time: " << (end - start) / 1.0e9 << " seconds\n";
-
-    sycl::free(data, q);
 
     return 0;
 }

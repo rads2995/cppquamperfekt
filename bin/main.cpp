30981c3f07bf1a9ff7a4d42cf1bc84aa1bd89e42
Rodrigo Duran <rads2995@gmail.com>
2025-09-10 13:57:15 -0400

    Simple elementwise ReLU example on AMD using Intel's oneDNN.

--- bin/main.cpp
+++ bin/main.cpp
@@ -1,62 +1,221 @@
 #include <vector>
 #include <iostream>
+#include <cmath>
+#include <stdexcept>
 
 #include <sycl/sycl.hpp>
+#include <oneapi/dnnl/dnnl.hpp>
+#include <oneapi/dnnl/dnnl_sycl.hpp>
 
 #include <cppquamperfekt/cppquamperfekt.hpp>
 
+// Read from handle, write to memory
+inline void write_to_dnnl_memory(void *handle, dnnl::memory &mem) {
+    dnnl::engine eng = mem.get_engine();
+    size_t size = mem.get_desc().get_size();
+
+    if (!handle) throw std::runtime_error("handle is nullptr.");
+
+#ifdef DNNL_WITH_SYCL
+    bool is_cpu_sycl = (DNNL_CPU_RUNTIME == DNNL_RUNTIME_SYCL
+            && eng.get_kind() == dnnl::engine::kind::cpu);
+    bool is_gpu_sycl = (DNNL_GPU_RUNTIME == DNNL_RUNTIME_SYCL
+            && eng.get_kind() == dnnl::engine::kind::gpu);
+    if (is_cpu_sycl || is_gpu_sycl) {
+        auto mkind = dnnl::sycl_interop::get_memory_kind(mem);
+        if (mkind == dnnl::sycl_interop::memory_kind::buffer) {
+            auto buffer = dnnl::sycl_interop::get_buffer<uint8_t>(mem);
+            auto dst = buffer.get_host_access();
+            uint8_t *dst_ptr = dst.get_pointer();
+            if (!dst_ptr)
+                throw std::runtime_error("get_pointer returned nullptr.");
+            for (size_t i = 0; i < size; ++i)
+                dst_ptr[i] = ((uint8_t *)handle)[i];
+        } else {
+            assert(mkind == dnnl::sycl_interop::memory_kind::usm);
+            uint8_t *dst_ptr = (uint8_t *)mem.get_data_handle();
+            if (!dst_ptr)
+                throw std::runtime_error("get_data_handle returned nullptr.");
+            if (is_cpu_sycl) {
+                for (size_t i = 0; i < size; ++i)
+                    dst_ptr[i] = ((uint8_t *)handle)[i];
+            } else {
+                auto sycl_queue
+                        = dnnl::sycl_interop::get_queue(dnnl::stream(eng));
+                sycl_queue.memcpy(dst_ptr, handle, size).wait();
+            }
+        }
+        return;
+    }
+#endif
+#if DNNL_GPU_RUNTIME == DNNL_RUNTIME_OCL
+    if (eng.get_kind() == dnnl::engine::kind::gpu) {
+        void *mapped_ptr = mem.map_data();
+        if (mapped_ptr) std::memcpy(mapped_ptr, handle, size);
+        mem.unmap_data(mapped_ptr);
+        return;
+    }
+#endif
+
+    if (eng.get_kind() == dnnl::engine::kind::cpu) {
+        uint8_t *dst = static_cast<uint8_t *>(mem.get_data_handle());
+        if (!dst) throw std::runtime_error("get_data_handle returned nullptr.");
+        for (size_t i = 0; i < size; ++i)
+            dst[i] = ((uint8_t *)handle)[i];
+        return;
+    }
+
+    assert(!"not expected");
+}
+
+inline void read_from_dnnl_memory(void *handle, dnnl::memory &mem) {
+    dnnl::engine eng = mem.get_engine();
+    size_t size = mem.get_desc().get_size();
+
+    if (!handle) throw std::runtime_error("handle is nullptr.");
+
+#ifdef DNNL_WITH_SYCL
+    bool is_cpu_sycl = (DNNL_CPU_RUNTIME == DNNL_RUNTIME_SYCL
+            && eng.get_kind() == dnnl::engine::kind::cpu);
+    bool is_gpu_sycl = (DNNL_GPU_RUNTIME == DNNL_RUNTIME_SYCL
+            && eng.get_kind() == dnnl::engine::kind::gpu);
+    if (is_cpu_sycl || is_gpu_sycl) {
+        auto mkind = dnnl::sycl_interop::get_memory_kind(mem);
+        if (mkind == dnnl::sycl_interop::memory_kind::buffer) {
+            auto buffer = dnnl::sycl_interop::get_buffer<uint8_t>(mem);
+            auto src = buffer.get_host_access();
+            uint8_t *src_ptr = src.get_pointer();
+            if (!src_ptr)
+                throw std::runtime_error("get_pointer returned nullptr.");
+            for (size_t i = 0; i < size; ++i)
+                ((uint8_t *)handle)[i] = src_ptr[i];
+        } else {
+            assert(mkind == dnnl::sycl_interop::memory_kind::usm);
+            uint8_t *src_ptr = (uint8_t *)mem.get_data_handle();
+            if (!src_ptr)
+                throw std::runtime_error("get_data_handle returned nullptr.");
+            if (is_cpu_sycl) {
+                for (size_t i = 0; i < size; ++i)
+                    ((uint8_t *)handle)[i] = src_ptr[i];
+            } else {
+                auto sycl_queue
+                        = dnnl::sycl_interop::get_queue(dnnl::stream(eng));
+                sycl_queue.memcpy(handle, src_ptr, size).wait();
+            }
+        }
+        return;
+    }
+#endif
+#if DNNL_GPU_RUNTIME == DNNL_RUNTIME_OCL
+    if (eng.get_kind() == dnnl::engine::kind::gpu) {
+        void *mapped_ptr = mem.map_data();
+        if (mapped_ptr) std::memcpy(handle, mapped_ptr, size);
+        mem.unmap_data(mapped_ptr);
+        return;
+    }
+#endif
+
+    if (eng.get_kind() == dnnl::engine::kind::cpu) {
+        uint8_t *src = static_cast<uint8_t *>(mem.get_data_handle());
+        if (!src) throw std::runtime_error("get_data_handle returned nullptr.");
+        for (size_t i = 0; i < size; ++i)
+            ((uint8_t *)handle)[i] = src[i];
+        return;
+    }
+
+    assert(!"not expected");
+}
+
 int main() {
 
-    constexpr int size = 1'000'000;
-    
-    sycl::device d;
-    try {
-        d = sycl::device(sycl::gpu_selector_v);
-    } catch (sycl::exception const& e) {
-        d = sycl::device(sycl::cpu_selector_v);
-    }
+    dnnl::engine engine {dnnl::engine::kind::gpu, 0};
 
-    sycl::property_list properties{sycl::property::queue::enable_profiling()};
-    sycl::queue q = sycl::queue(d, properties);
+    dnnl::stream engine_stream {engine};
 
-    std::cout
-        << "Platform: "
-        << q.get_device().get_platform().get_info<sycl::info::platform::name>()
-        << "\n";
+    // Create a 4D tensor in NHWC format
+    constexpr int N = 1, H = 13, W = 13, C = 3;
 
+    // Compute physical strides for each dimension
+    constexpr int stride_N = H * W * C;
+    constexpr int stride_H = W * C;
+    constexpr int stride_W = C;
+    constexpr int stride_C = 1;
 
-    // USM allocator for data of type int in shared memory
-    using vec_alloc = sycl::usm_allocator<int, sycl::usm::alloc::shared>;
+    // Auxiliary function that maps logical index to the physical offset
+    auto offset = [=](int n, int h, int w, int c) {
+        return n * stride_N + h * stride_H + w * stride_W + N * stride_C;
+    };
 
-    // Create allocator for device associated with queue
-    vec_alloc allocator(q);
+    constexpr int image_size = N * H * W * C;
 
-    // Create std::vector using this allocator
-    std::vector<int, vec_alloc> a(size, allocator), b(size, allocator), c(size, allocator);
+    // Allocato a buffer for the image
+    std::vector<float> image(image_size);
 
-    // Get pointer(s) to vector(s) data for access in kernel
-    auto A = a.data();
-    auto B = b.data();
-    auto C = c.data();
+    // Initialize the image with some values
+    for (int n = 0; n < N; ++n)
+        for (int h = 0; h < H; ++h)
+            for (int w = 0; w < W; ++w)
+                for (int c = 0; c < C; ++c) {
+                    int off = offset(
+                            n, h, w, c); // Get the physical offset of a pixel
+                    image[off] = -std::cos(off / 10.f);
+                }
 
-    for (int i = 0; i < size; i++) {
-        a[i] = i;
-        b[i] = i;
-        c[i] = i;
-    }
+    auto src_md = dnnl::memory::desc(
+            {N, C, H, W}, // logical dims, the order is defined by a primitive
+            dnnl::memory::data_type::f32,   // tensor's data type
+            dnnl::memory::format_tag::nhwc  // memory format, NHWC in this case
+    );
 
-    sycl::event event = q.submit([&](sycl::handler &h) {
-        h.parallel_for(sycl::range<1>(size),
-            [=](sycl::id<1> idx) {
-                C[idx] = A[idx] + B[idx];
+    // src_mem contains a copy of image after write_to_dnnl_memory function
+    auto src_mem = dnnl::memory(src_md, engine);
+    write_to_dnnl_memory(image.data(), src_mem);
+
+    auto dst_mem = dnnl::memory(src_md, engine);
+
+    auto relu_pd = dnnl::eltwise_forward::primitive_desc(
+        engine, // an engine the primitive will be created for
+        dnnl::prop_kind::forward_inference, dnnl::algorithm::eltwise_relu,
+        src_md, // source memory descriptor for an operation to work on
+        src_md, // destination memory descriptor for an operation to work on
+        0.f, // alpha parameter means negative slope in case of ReLU
+        0.f // beta parameter is ignored in case of ReLU
+    );
+
+    // ReLU primitive
+    auto relu = dnnl::eltwise_forward(relu_pd);
+
+    relu.execute(engine_stream, // The execution stream
+            {
+                    // A map with all inputs and outputs
+                    {DNNL_ARG_SRC, src_mem}, // Source tag and memory obj
+                    {DNNL_ARG_DST, dst_mem}, // Destination tag and memory obj
             });
-    });
-    event.wait();
-    
-    auto start = event.get_profiling_info<sycl::info::event_profiling::command_start>();
-    auto end = event.get_profiling_info<sycl::info::event_profiling::command_end>();
-    
-    std::cout << "Elapsed time: " << (end - start) / 1.0e9 << " seconds\n";
+
+    // Wait the stream to complete the execution
+    engine_stream.wait();
+
+    std::vector<float> relu_image(image_size);
+    read_from_dnnl_memory(relu_image.data(), dst_mem);
+
+    // Check the results
+    for (int n = 0; n < N; ++n)
+        for (int h = 0; h < H; ++h)
+            for (int w = 0; w < W; ++w)
+                for (int c = 0; c < C; ++c) {
+                    int off = offset(
+                            n, h, w, c); // get the physical offset of a pixel
+                    float expected = image[off] < 0
+                            ? 0.f
+                            : image[off]; // expected value
+                    if (relu_image[off] != expected) {
+                        std::cout << "At index(" << n << ", " << c << ", " << h
+                                  << ", " << w << ") expect " << expected
+                                  << " but got " << relu_image[off]
+                                  << std::endl;
+                        throw std::logic_error("Accuracy check failed.");
+                    }
+                }
 
     return 0;
 }
